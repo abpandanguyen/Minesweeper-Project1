6da24c3c0c24d0082be8f5f65b645bf025fe419a
abpandanguyen <abpandanguyen@gmail.com>
2022-07-02 17:55:34 -0700

    setminesandclick

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 const mineCount = 10;
 const columns = 8;
 const rows = 8;
+const mineLocation = [];
 
 
 
 /*----- app's state (variables) -----*/
-let minefield; // 
-let gameStatus; // null is game in play, -1 loss, 1 win
+var gameStatus = null; // null is game in play, -1 loss, 1 win
 
 
 
@@ -28,25 +28,40 @@ let gameStatus; // null is game in play, -1 loss, 1 win
 init();
 
 function init() {
-    for (let r = 0; r < rows; r++) {
+    for (let r = 0; r < rows; r++) { 
         let row = [];
-        for (let c = 0; c < columns; c++) {
+        for (let c = 0; c < columns; c++) { // double for loop to create grid
 
 
-        let tile = document.createElement("div");
-        tile.id = r.toString() + "-" + c.toString(); // sets the coordinates
-        tile.addEventListener("click", clickTile);
-        document.getElementById("minefield").append(tile);
-        row.push(tile);
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString(); // sets the coordinates
+            tile.addEventListener("click", clickTile); 
+            document.getElementById("minefield").append(tile); // attaches div to minefield 
+            row.push(tile); //
         }
      }
-        
+    setMinefield();    
     render();
+};
+
+function setMinefield() {
+    mineLocation.push("2-2");
+    mineLocation.push("2-3");
+    mineLocation.push("2-4");
+    mineLocation.push("1-1");
+    mineLocation.push("1-2");
+
 };
 
 
 function clickTile() {
-    console.log ("Working");
+    // console.log ("Working");
+    let tile = this;
+
+    if (mineLocation.includes(tile.id)) {
+        gameStatus = -1;
+        console.log ("Game Over");
+    }
 
 };
 
