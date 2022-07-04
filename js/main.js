22a925456aa18b97b0c144b579aad5164a4c73b0
abpandanguyen <abpandanguyen@gmail.com>
2022-07-04 15:51:32 -0700

    testingcheckminecell

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 const mineCount = 10;
 const columns = 8;
 const rows = 8;
-const mineLocation = [];
-
 
 
 /*----- app's state (variables) -----*/
@@ -30,7 +28,7 @@ function init() {
     for (let r = 0; r < rows; r++) { 
         for (let c = 0; c < columns; c++) { // double for loop to fill grid
             let id = r.toString() + "-" + c.toString(); // sets the coordinates 
-            let tile = { id: id, isMine: false, adjacentMineCount: 0, flagged: false, clicked: false};
+            let tile = { id: id, isMine: false, adjacentMineCount: 0, flagged: false, clicked: false}; //id property is connected to the html grid
             minefieldTile.push(tile);
         }
     }
@@ -68,9 +66,53 @@ function clickTile(evt) {
     
     if (minefieldTile[index].isMine == true) {
         evt.target.style.backgroundColor = "red";
+        showMines();
         console.log ("Game Over"); //checking if clicking on a mined tile works
+    } else {
+        checkMine(evt);
     }
 };
+
+function showMines() {
+    let mines = minefieldTile.filter(function(value) {
+        return value.isMine == true
+    });
+        for (let x = 0; x < mines.length; x++) {
+            let div = document.getElementById(mines[x].id); //using newly filtered array and using for loop to set all backgrounds to red aka mine
+            div.style.backgroundColor = "red";
+        }
+    
+}   
+
+function checkMine(evt) {
+    let mineCounter = 0;
+    let id = evt.target.id;
+    let coord = id.split('-');
+    let row = coord[0];
+    let column = coord[1];
+    // console.log (coord);
+    let newId = (row-1).toString() + "-" + (column-1).toString();
+    let newTile = minefieldTile.find(function(value) {
+        return value.id == newId;
+    });
+
+    if (newTile != undefined) { //guard against undefined
+        if (newTile.isMine == true ) {
+            mineCounter ++;
+        
+        }
+    }   
+    console.log (mineCounter);
+}
+
+function checkTile() {
+    let mines = minefieldTile.filter(function(value) {
+        return value.isMine == true
+    });
+};
+    
+
+
 
 function render() {
 
