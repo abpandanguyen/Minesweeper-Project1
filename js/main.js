009dcd328a27ebe74faecd28c04bf3e2efc04cb8
abpandanguyen <abpandanguyen@gmail.com>
2022-07-03 18:16:21 -0700

    revampgame

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const mineLocation = [];
 
 /*----- app's state (variables) -----*/
 var gameStatus = null; // null is game in play, -1 loss, 1 win
-
+var minefieldTile = [];
 
 
 /*----- cached element references -----*/
@@ -18,7 +18,7 @@ var gameStatus = null; // null is game in play, -1 loss, 1 win
 
 
 /*----- event listeners -----*/
-
+document.getElementById("minefield").addEventListener('click', clickTile);
 
 
 
@@ -28,53 +28,48 @@ init();
 
 function init() {
     for (let r = 0; r < rows; r++) { 
-        let row = [];
-        for (let c = 0; c < columns; c++) { // double for loop to create grid
-
-
-            let tile = document.createElement("div"); // creating tile div
-            tile.id = r.toString() + "-" + c.toString(); // sets the coordinates
-            tile.addEventListener("click", clickTile); 
-            document.getElementById("minefield").append(tile); // attaches div to minefield 
-            row.push(tile); //
+        for (let c = 0; c < columns; c++) { // double for loop to fill grid
+            let id = r.toString() + "-" + c.toString(); // sets the coordinates 
+            let tile = { id: id, isMine: false, adjacentMineCount: 0, flagged: false, clicked: false};
+            minefieldTile.push(tile);
         }
-     }
+    }
     setMinefield();    
     render();
 };
 
 function setMinefield() {
-    // mineLocation.push("2-2"); // setting 'mines' at respective coordinates to test
-    // mineLocation.push("2-3");
-    // mineLocation.push("2-4");
-    // mineLocation.push("1-1");
-    // mineLocation.push("1-2");
 
     let minesRemain = mineCount // declaring new variable
     while (minesRemain > 0) { // while makes sure the loop continues until condition met
-        let r = Math.floor(Math.random()*rows);
-        let c = Math.floor(Math.random()*columns);
+        let r = Math.floor(Math.random()*rows); 
+        let c = Math.floor(Math.random()*columns); //calculating and assigning random r and c variables to put as 'coordinates'
         let mine = r.toString() + "-" + c.toString(); // setting mine variable like above and setting coordinate
-        if (mineLocation.includes(mine) == false) { // setting condition to avoid overlapping of bombs
-            mineLocation.push(mine); // if the array doesn't have that string in it, then it will add in a new mine to the array
-             minesRemain --; // preventing infinite loop
+        let index = minefieldTile.findIndex(function(value) { 
+            return value.id == mine; // setting index variable to the random mine value we found above and seeing if they are equal
+        });
+        
+        if (minefieldTile[index].isMine == false) {
+            minesRemain --;
+            minefieldTile[index].isMine = true; // function found index so we can modify array value appropriately
         }
             
 
     }
-    console.log (mineLocation);
+    console.log (minefieldTile);
 };
 
 
-function clickTile() {
-    // console.log ("Working");
-    let tile = this;
-
-    if (mineLocation.includes(tile.id)) {
-        gameStatus = -1;
+function clickTile(evt) {
+    let tile = evt.target;
+    let index = minefieldTile.findIndex(function(value) { 
+        return value.id == tile.id; // setting index variable to the random mine value we found above and seeing if they are equal
+    });
+    
+    if (minefieldTile[index].isMine == true) {
+        evt.target.style.background = "/Users/anthonynguyen/code/Minesweeper-Project1/imgs/mine.png";
         console.log ("Game Over"); //checking if clicking on a mined tile works
     }
-
 };
 
 function render() {
